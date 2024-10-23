21809cb23ab508f79eb99c85913965a27269ccae
Max Medina <103536634+maxmedina18@users.noreply.github.com>
2024-10-23 00:55:07 -0500

    Create test_setup.py

---
+++ test_setup.py
@@ -0,0 +1,11 @@
+import torch
+import librosa
+import numpy as np
+import matplotlib.pyplot
+import soundfile as sf
+
+print("PyTorch version:", torch.__version__)
+print("Librosa version:", librosa.__version__)
+print("Numpy version:", np.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("Soundfile version:", sf.__version__)
